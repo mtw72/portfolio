9755b1da80770e5f8d5d1a11e6b2dc675b09f764
mtw72 <mtwong72@gmail.com>
2024-08-09 16:34:42 +0100

    updated list images

--- img.js
+++ img.js
@@ -38,41 +38,20 @@ const sharp = require("sharp");
                 .toFile(outputImagePath);
         }
 
-        // // Hero images
-        // resizeImage(imgRawSRC + "home-bg-sm" + jpeg, imgResizedDEST + "home-bg-sm" + jpeg, 450, 450 * 4 / 3);
-        // resizeImage(imgRawSRC + "home-bg-md" + jpeg, imgResizedDEST + "home-bg-md" + jpeg, 931, 931 * 4 / 5);
-        // resizeImage(imgRawSRC + "home-bg-lg" + jpeg, imgResizedDEST + "home-bg-lg" + jpeg, 1500, 1500 * 9 / 16);
+        // copy the image from the raw folder to the resize folder
+        sharp(imgRawSRC + "arrow-right" + png)
+            .toFile(imgResizedDEST + "arrow-right" + png);
+
+        sharp(imgRawSRC + "pen" + png)
+            .toFile(imgResizedDEST + "pen" + png);
 
         // OG Image: use images with a 1.91:1 ratio and minimum recommended dimensions of 1200x630 for optimal clarity across all devices.
         // Twitter Card Image: requirements vary from time to time. Follow the OG Image. Image must be less than 1MB in size.
-        // unchanged but copy the image from the raw folder to the resize folder
         sharp(imgRawSRC + "meta-image" + png)
             .toFile(imgResizedDEST + "meta-image" + png);
 
-        // // OG Image: use images with a 1.91:1 ratio and minimum recommended dimensions of 1200x630 for optimal clarity across all devices.
-        // resizeImage(imgRawSRC + "og-image" + jpeg, imgResizedDEST + "og-image" + jpeg, 1200, 630);
-
-        // //Food Images - same size on different viewport widths
-        // const foodImageNames = [
-        //     "salmon-spaghetti",
-        //     "satay-chicken-skewers",
-        //     "thai-pineapple-fried-rice"
-        // ];
-
-        // foodImageNames.forEach(imageName => {
-        //     resizeImage(imgRawSRC + imageName + jpeg, imgResizedDEST + imageName + jpeg, 380, 501);
-        // });
-
-        // // Reservation Photo - different sizes on different viewport widths
-        // resizeImage(imgRawSRC + "r2place-entrance" + jpeg, imgResizedDEST + "r2place-entrance-sm" + jpeg, 425, 425 / 1.68);
-        // resizeImage(imgRawSRC + "r2place-entrance" + jpeg, imgResizedDEST + "r2place-entrance-md" + jpeg, 600, 600 / 1.68);
-
         // Carousel Images - different sizes on different viewport widths
-        // resizeImage(imgRawSRC + "r2-googletest" + jpg, imgResizedDEST + "r2-googletest-xxxs" + jpeg, 282, 324.13);
-        // resizeImage(imgRawSRC + "r2-googletest" + jpg, imgResizedDEST + "r2-googletest-xxs" + jpeg, 414, 475.86);
         resizeImage(imgRawSRC + "r2-googletest" + jpg, imgResizedDEST + "r2-googletest" + jpeg, 500, 574.7);
-        // resizeImage(imgRawSRC + "wireframe-iPad" + jpg, imgResizedDEST + "wireframe-iPad-xxxs" + jpeg, 282, 324.13);
-        // resizeImage(imgRawSRC + "wireframe-iPad" + jpg, imgResizedDEST + "wireframe-iPad-xxs" + jpeg, 414, 475.86);
         resizeImage(imgRawSRC + "wireframe-iPad" + jpg, imgResizedDEST + "wireframe-iPad" + jpeg, 500, 574.7);
 
         console.log('IMAGES RESIZE — completed!');
